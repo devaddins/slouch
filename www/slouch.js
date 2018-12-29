c552508bc6ac18dceeb0de18cb3cbdf6017ed895
Ben McGee <ben@benmcgee.com>
2018-12-28 20:52:30 -0700

    git bs

--- www/slouch.js
+++ www/slouch.js
@@ -2,24 +2,24 @@ var exec = require('cordova/exec');
 
 exports.get = function(path) {
     return new Promise(function(res, rej){
-        exec(res, rej, 'slouch', 'get', path);
+        exec(res, rej, 'slouch', 'get', [path]);
     });
 };
 
 exports.put = function(path, data) { 
     return new Promise(function(res, rej){
-        exec(res, rej, 'slouch', 'put', path, data);
+        exec(res, rej, 'slouch', 'put', [path, data]);
     });
 };
 
 exports.post = function(path, data) {
     return new Promise(function(res, rej){
-        exec(res, rej, 'slouch', 'post', path, data);
+        exec(res, rej, 'slouch', 'post', [path, data]);
     });
 };
 
 exports.delete = function(path) {
     return new Promise(function(res, rej){
-        exec(res, rej, 'slouch', 'delete', path);
+        exec(res, rej, 'slouch', 'delete', [path]);
     });
 };
